66181960fe4e3dda5a8bef49ecf36903fda237ba
Mike Fikes <mike@fikesfarm.com>
2021-10-31 10:19:21 -0400

    Add a Prologix driver

--- battery_discharge.py
+++ battery_discharge.py
@@ -1,32 +1,12 @@
-import serial
+import prologix
 import time
 
-def command(ser, cmd):
-    ser.write(bytes(cmd + "\n", "utf-8"))
-    # Give a little time for the command to be executed
-    time.sleep(0.010)
+def setup_instrument(gpib, addr):
+    gpib.command(addr, "*rst")
+    print(gpib.query(addr, "*idn?"))
 
-def read(ser):
-    command(ser, "++read eoi")
-    return ser.read(100).decode("utf-8")
-
-def setup_prologix(addr):
-    ser = serial.Serial('/dev/cu.usbserial-PXEFMYB9', timeout=1)
-    command(ser, "++addr " + str(addr));
-    command(ser, "++mode 1")
-    command(ser, "++auto 0")
-    command(ser, "++eos 3")
-    command(ser, "++eoi 1");
-    return ser
-
-def setup_instrument(ser):
-    command(ser, "*rst")
-    command(ser, "*idn?")
-    print(read(ser))
-
-def read_voltage(ser):
-    command(ser, ":read?")
-    s = read(ser)
+def read_voltage(gpib, addr):
+    s = gpib.query(addr, ":read?")
     vals = s.split(",")
     voltage = float(vals[0])
     return voltage
@@ -45,21 +25,27 @@ if __name__ == '__main__':
     cutoff_voltage = 1.0
     measurement_interval = 10
     recovery_interval = 2
-    
-    ser = setup_prologix(26)
-    setup_instrument(ser)
 
+    port = '/dev/cu.usbserial-PXEFMYB9'
+    addr = 26
+    
+    gpib = prologix.Prologix(port)
+    setup_instrument(gpib, addr)
+
+    time.sleep(100);
+    1/0
+    
     # Set high impedance off state so battery can't drive SMU
-    command(ser, ":output:smode himp")
+    gpib.command(addr, ":output:smode himp")
 
     # Source current, measure voltage
     # TODO Do this via commands rather than simulated key presses
-    command(ser, ":system:key 19");
-    command(ser, ":system:key 15");
+    gpib.command(addr, ":system:key 19")
+    gpib.command(addr, ":system:key 15")
 
     # Initially set to not drain and turn on output
-    command(ser, ":source:current 0")
-    command(ser, ":output:state on")
+    gpib.command(addr, ":source:current 0")
+    gpib.command(addr, ":output:state on")
 
     voc_esr_table = []
     
@@ -67,14 +53,14 @@ if __name__ == '__main__':
     while not(done):
         
         # Drain the battery for the measurement interval
-        command(ser, ":source:current -" + str(drain_current))
+        gpib.command(addr, ":source:current -" + str(drain_current))
         time.sleep(measurement_interval)
-        voltage = read_voltage(ser)
+        voltage = read_voltage(gpib, addr)
         
         # Measure open circuit voltage
-        command(ser, ":source:current 0")
+        gpib.command(addr, ":source:current 0")
         time.sleep(recovery_interval);
-        voc = read_voltage(ser);
+        voc = read_voltage(gpib, addr);
 
         esr = calculate_esr(voltage, voc, drain_current);
 
@@ -85,8 +71,7 @@ if __name__ == '__main__':
             done = True
 
     # Now shut off instrument and close comm
-    command(ser, ":output:state off")
-    ser.close()
+    gpib.command(addr, ":output:state off")
 
     print("Cutoff voltage reached. Dumping battery charge model CSV.\n")
     
