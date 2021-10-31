9b422c9bbf04eb2d334e6100c705eb7d3c44dad4
Mike Fikes <mike@fikesfarm.com>
2021-10-31 16:36:44 -0400

    Discharge model persistence (#3)

--- battery_discharge.py
+++ battery_discharge.py
@@ -1,5 +1,6 @@
 from prologix import Prologix
 from keithley_2400 import Keithley_2400
+import discharge_model
 import time
 
 def calculate_esr(voltage, voc, current):
@@ -9,6 +10,9 @@ def lookup_voc_esr(soc, voc_esr_table):
     index = round((len(voc_esr_table)-1)*(100-soc)/100)
     return voc_esr_table[index]
 
+def create_discharge_model(voc_esr_table):
+    return lambda soc : lookup_voc_esr(soc, voc_esr_table)
+
 if __name__ == '__main__':
 
     # Main parameters
@@ -17,6 +21,7 @@ if __name__ == '__main__':
     measurement_interval = 10
     recovery_interval = 2
     load_interval = 0.1
+    discharge_model_filename = 'foo.csv'
 
     port = '/dev/cu.usbserial-PXEFMYB9'
     
@@ -62,10 +67,8 @@ if __name__ == '__main__':
         time.sleep(measurement_interval)
 
 
-    print("Cutoff voltage reached. Dumping battery charge model CSV.\n")
+    print("Writing battery charge model to " + discharge_model_filename)
     
-    # Print CSV for the battery charge model
-    print("SOC,VOC,ESR")
-    for soc in range(101):
-        [voc, esr] = lookup_voc_esr(soc, voc_esr_table)
-        print(str(soc) + "," + str(voc) + "," + str(esr))
+    # Dump the battery charge model
+    discharge_model.write(discharge_model_filename, create_discharge_model(voc_esr_table))
+
